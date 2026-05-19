208ac1ad871243de6cb8a6d4aac0513fb75ac8dd
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-05-19 14:28:19 +0200

    first commit

---
+++ src/main/java/com/final_project/workgroup_final_project/WorkgroupFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.final_project.workgroup_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkgroupFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkgroupFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/final_project/workgroup_final_project/WorkgroupFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.final_project.workgroup_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkgroupFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
